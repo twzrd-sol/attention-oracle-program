9baf22fb0fcdc4edb2a177e956b370d77cd4b3cb
twzrd-xyz <33047129+twzrd-sol@users.noreply.github.com>
2025-11-27 19:58:19 +0000

    chore: remove filter script [allow-direct]

--- msg_cb.py
+++
@@ -1,5 +0,0 @@
-def message_callback(commit):
-    import re
-    msg = commit.message.decode('utf-8', errors='ignore')
-    new = re.sub(r'(?i)\bmilo\b', 'cleanup', msg)
-    commit.message = new.encode('utf-8')
